93e9533de678cca49027eb0c05bd8b50b7b8ae8e
Hothuankhang <hothuankhang.2511@gmail.com>
2021-12-20 16:00:12 +0700

    Message

--- src/App.js
+++ src/App.js
@@ -7,15 +7,19 @@ import db from './firebase_config';
 import { useState } from 'react';
 import { useEffect } from 'react';
 
+
 function App() {
   const [todoInput,setTodoInput] = useState('');
   const [todos, setTodos] = useState([]);
   const [todoEditing, setTodoEditing] = useState(null)
   const [editingText, setEditingText] = useState("")
   const [value, setValue]=useState([])
+  const [type, setType] = useState('All')
+
 
   useEffect(()=>{
     getTodos();
+    
   },[])
   
 
@@ -39,6 +43,7 @@ function App() {
     )
   }
 
+
   function addTodo(e){
     // Chặn refresh
     e.preventDefault();
@@ -52,19 +57,57 @@ function App() {
       });
     }
     setTodoInput('');
-    
+    console.log('Chạy add')
   }
 
   // Thay đổi trạng thái
   function changeStatus(id,inprogress){
     db.collection("todos").doc(id).update({
         inprogress: !inprogress
-    })
+    }).then(()=>{
+      if(type === 'Done')
+      {
+        for(let i =0; i<value.length;i++){
+          if(value[i].id === id){
+            value[i].inprogress = true
+            console.log(todos)
+          }
+      }
+        handleDone();
+      }
+    if(type === 'OnProgress'){
+      for(let i =0; i<value.length;i++){
+        if(value[i].id === id){
+          value[i].inprogress = false
+          console.log(todos)
+        }
+      }
+      handleOnProgress()
+    }    
+    }
+    )
 }
 
   // Xóa
   function deleteTodo(id){
-      db.collection("todos").doc(id).delete();
+    db.collection("todos").doc(id).delete().then(() =>{
+      const a= [];
+    for(let i =0; i<value.length;i++){
+      
+        if(value[i].id !== id){
+          
+          a.push(value[i]);
+        }
+    }
+    setValue(a);
+
+    const b= [];
+    for(let i =0; i<todos.length;i++){
+        if(todos[i].id !== id){
+            b.push(todos[i]);
+          }
+    }
+    setTodos(b);})
   }
 
   // Sửa
@@ -72,14 +115,34 @@ function App() {
    if(editingText!=="")
     db.collection("todos").doc(id).update({
       todo: editingText,
-    });
-
+    }).then(()=>{
+      if(type === 'Done'){
+        for(let i =0; i<value.length;i++){
+            if(value[i].id === id){
+              value[i].todo = editingText 
+              console.log(todos)
+            }
+        }
+        handleDone();
+      }
+    if(type === 'OnProgress'){
+      for(let i =0; i<value.length;i++){
+        if(value[i].id === id){
+          value[i].todo = editingText 
+          console.log(todos)
+        }
+    }
+    }    
+      handleOnProgress()
+    
+    })
     setTodoEditing(null);
     setEditingText("");
   }
 
   function handleAll(){
     setValue(todos);
+    setType('All');
 }
 
 function handleDone(){
@@ -89,6 +152,7 @@ function handleDone(){
             a.push(todos[i]);
     }
     setValue(a);
+    setType('Done');
 }
 
 function handleOnProgress(){
@@ -98,11 +162,10 @@ function handleOnProgress(){
             a.push(todos[i]);
     }
     setValue(a);
+    setType('OnProgress');
 }
 
 function onKeyDown(e){
-  console.log(e.key)
-  console.log(todoEditing)
 
   if(e.key === 'Enter'){
     editTodo(todoEditing);
@@ -110,6 +173,7 @@ function onKeyDown(e){
 }
 
   return (
+    
     <div className="App">
       <header className="App-header">
         <h1>Todo-list</h1>
@@ -124,11 +188,21 @@ function onKeyDown(e){
           <button onClick={addTodo}>Add</button>
         </form>
         <div className='tabs'>
-          <button className="tabs__choice" onClick={handleAll}>All</button>
-            <button className="tabs__choice" onClick={handleDone}>Done</button>
-            <button className="tabs__choice" onClick={handleOnProgress}>On progress</button>
+          <button className={(type === 'All') ? 
+            'tabs__choice tabs__choice--choose' 
+            : 'tabs__choice '} 
+            onClick={handleAll}>All</button>
+            <button className={(type === 'Done') ? 
+              'tabs__choice tabs__choice--choose' 
+            : 'tabs__choice '} 
+            onClick={handleDone}>Done</button>
+            <button className={(type === 'OnProgress') ? 
+              'tabs__choice tabs__choice--choose' 
+            : 'tabs__choice '} 
+            onClick={handleOnProgress}>On progress</button>
           </div>
-        {value.map((todo) => (
+        {
+        value.map((todo) => (
           <div 
           key={todo.id} 
           className={todo.inprogress ? 'todo__row ' 
@@ -147,10 +221,12 @@ function onKeyDown(e){
             ) 
             : 
             (
-              <div 
+              <input
               className="todo__row__text" 
               onClick={()=>changeStatus(todo.id,todo.inprogress)}
-              >{todo.todo}</div>
+              value={todo.todo}
+              readOnly
+              />
             )}
             {todoEditing === todo.id ? 
             (
@@ -172,7 +248,7 @@ function onKeyDown(e){
                 >Delete</button>
                 <button 
                 className="todo__row__btn"
-                onClick={() => setTodoEditing(todo.id)}
+                onClick={() => setTodoEditing(todo.id, todo.todo,todo.inprogress)}
                 >Edit
                 </button>  
               </div>
